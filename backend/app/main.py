f684e85fe06ed27bf674ff8ea14ac216b51a77fd
Arkadiusz Juszczyk <arkadiuszjuszczyk@hotmail.com>
2023-11-04 21:07:20 +0100

    feat. docker setup for fastapi

---
+++ backend/app/main.py
@@ -0,0 +1,19 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/hey")
+def read_root():
+    return {"Hello": "omg!"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
