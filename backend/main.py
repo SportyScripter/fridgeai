da2ff360bae7fdaa1635fbdf25206b7743f1adf0
Arkadiusz Juszczyk <arkadiuszjuszczyk@hotmail.com>
2023-11-14 22:06:20 +0100

    chores

--- backend/main.py
+++ backend/main.py
@@ -13,24 +13,15 @@ from db.session import engine, db
 from db.base import Base, Product
 
 def create_tables():         
-	Base.metadata.create_all(bind=engine)
-        
+	Base.metadata.create_all(bind=engine)     
 
 def start_application():
-    app = FastAPI(title=config.PROJECT_NAME,version=config.PROJECT_VERSION)
-    create_tables()
-    return app
-
+   app = FastAPI(title=config.PROJECT_NAME,version=config.PROJECT_VERSION)
+   create_tables()
+   return app
 
 app = start_application()
 
-#Creating a cursor object using the cursor() method
 @app.get("/products")
 def get_products():
    return db.query(Product).all()
-
-# @app.get("/pure_list")
-# def get_List():
-#    cursor.execute('''SELECT * from Products''')
-#    result = cursor.fetchall()
-#    return result
