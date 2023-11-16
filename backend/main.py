05e803906e3b220c43f52abaccc19715ab668b8b
Arkadiusz Juszczyk <arkadiuszjuszczyk@hotmail.com>
2023-11-16 18:22:45 +0100

    feat. sqlalchemy and env handlers (#23)
    * feat. sqlalchemy and env handlers
    
    * removed alembic
    
    * chores
    
    * restored pgadmin
    
    * fix for server env
    
    * fix for products list

---
+++ backend/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+
+from dotenv import load_dotenv
+
+from pathlib import Path
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+from fastapi import FastAPI
+from core.config import config
+from db.session import engine, db
+from db.base import Base, Product
+
+def create_tables():         
+	Base.metadata.create_all(bind=engine)     
+
+def start_application():
+   app = FastAPI(title=config.PROJECT_NAME,version=config.PROJECT_VERSION)
+   create_tables()
+   return app
+
+app = start_application()
+
+@app.get("/products")
+def get_products():
+   list = db.query(Product).all()
+
+   if list is None:
+      return {"message": "No products found"}
+   
+   return list
