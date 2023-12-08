9e97ab90ff9107656c17802e5d1f79771dcabf8b
SportyScripter <adrian.kacorzyk@edu.uekat.pl>
2023-12-08 18:27:03 +0100

    Added simply methods for removing and adding a product to the database

--- backend/db/base_class.py
+++ backend/db/base_class.py
@@ -1,5 +1,5 @@
 from typing import Any
-from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy.ext.declarative import declared_attr , declarative_base
 from sqlalchemy.orm import as_declarative
 
 @as_declarative()
